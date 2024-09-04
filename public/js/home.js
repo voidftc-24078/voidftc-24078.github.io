318e666bef2d9752443d573df47ed9a6366832df
Waverider <unstinkable919@gmail.com>
2024-09-04 08:17:32 -0400

    organize

---
+++ public/js/home.js
@@ -0,0 +1,39 @@
+
+var aText = new Array(
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod finibus velit eget convallis. Duis ut vestibulum metus. Nullam vehicula dui eros, at pharetra nisl dapibus at. Aliquam eget elementum orci. Curabitur et quam quis felis ornare auctor. Maecenas ac eleifend nibh. Maecenas at nisi id urna eleifend hendrerit. Integer condimentum arcu eu faucibus suscipit. Suspendisse malesuada magna et diam fermentum posuere. Ut fermentum cursus feugiat. Suspendisse potenti. Etiam dolor metus, pulvinar at lacus eu, fermentum interdum mi. Nulla consequat dictum sagittis"
+    );
+    var iSpeed = 20; // time delay of print out
+    var iIndex = 0; // start printing array at this posision
+    var iArrLength = aText[0].length; // the length of the text array
+    var iScrollAt = 20; // start scrolling up at this many lines
+    var iTextPos = 0; // initialise text position
+    var sContents = ''; // initialise contents variable
+    var iRow; // initialise current row
+
+function typewriter()
+{
+    sContents =  ' ';
+    iRow = Math.max(0, iIndex-iScrollAt);
+    var destination = document.getElementById("typedtext");
+    
+    while ( iRow < iIndex ) {
+    sContents += aText[iRow++] + '<br />';
+    }
+    destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + '<span class="cursor">&block;</span>';
+
+    if ( iTextPos++ == iArrLength ) {
+    iTextPos = 0;
+    iIndex++;
+    
+    if ( iIndex != aText.length ) {
+    iArrLength = aText[iIndex].length;
+    setTimeout("typewriter()", 500);
+    }
+    } else {
+    setTimeout("typewriter()", iSpeed);
+    }
+    
+}
+
+
+typewriter();
--- public/js/main.js
+++ public/js/main.js
@@ -1,41 +1,30 @@
 
-// set up text to print, each item in array is new line
-
-var aText = new Array(
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod finibus velit eget convallis. Duis ut vestibulum metus. Nullam vehicula dui eros, at pharetra nisl dapibus at. Aliquam eget elementum orci. Curabitur et quam quis felis ornare auctor. Maecenas ac eleifend nibh. Maecenas at nisi id urna eleifend hendrerit. Integer condimentum arcu eu faucibus suscipit. Suspendisse malesuada magna et diam fermentum posuere. Ut fermentum cursus feugiat. Suspendisse potenti. Etiam dolor metus, pulvinar at lacus eu, fermentum interdum mi. Nulla consequat dictum sagittis"
-    );
-    var iSpeed = 20; // time delay of print out
-    var iIndex = 0; // start printing array at this posision
-    var iArrLength = aText[0].length; // the length of the text array
-    var iScrollAt = 20; // start scrolling up at this many lines
-     
-    var iTextPos = 0; // initialise text position
-    var sContents = ''; // initialise contents variable
-    var iRow; // initialise current row
-     
-    function typewriter()
-    {
-     sContents =  ' ';
-     iRow = Math.max(0, iIndex-iScrollAt);
-     var destination = document.getElementById("typedtext");
-     
-     while ( iRow < iIndex ) {
-      sContents += aText[iRow++] + '<br />';
-     }
-     destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + '<span class="cursor">&block;</span>';
-    
-     if ( iTextPos++ == iArrLength ) {
-      iTextPos = 0;
-      iIndex++;
-      
-      if ( iIndex != aText.length ) {
-       iArrLength = aText[iIndex].length;
-       setTimeout("typewriter()", 500);
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /* Loop through a collection of all HTML elements: */
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      /* Make an HTTP request using the attribute value as the file name: */
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /* Remove the attribute, and call this function once more: */
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
+        }
       }
-     } else {
-      setTimeout("typewriter()", iSpeed);
-     }
-     
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /* Exit the function: */
+      return;
     }
+  }
+}
     
-    typewriter();
+    
