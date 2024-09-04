a8ceadc62c227bd11e3f14693b0c0538eb80a625
Waverider <unstinkable919@gmail.com>
2024-09-04 11:43:51 -0400

    create gallery

---
+++ public/js/gallery.js
@@ -0,0 +1,22 @@
+// get list of images
+let images = document.querySelectorAll('img')
+// get wrapper element
+let wrapper = document.getElementById('wrapper')
+// get fullscreen image element
+let imgWrapper = document.getElementById('fullImg')
+// get close button element
+let closebutton = document.querySelector('span')
+
+images.forEach((img,index) => {
+    img.addEventListener("click", () => {
+        openImage(`img/image${index}.png`)
+    })
+})
+
+closebutton.addEventListener("click", () => (wrapper.style.display = 'none'))
+
+function openImage(img) {
+    wrapper.style.display = 'flex'
+    imgWrapper.src = img
+}
+
