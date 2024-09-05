1902fd7e5d9ea9819b1a09c757c09096d77b3759
Waverider <unstinkable919@gmail.com>
2024-09-05 11:24:10 -0400

    e

--- public/js/typewriter.js
+++ public/js/typewriter.js
@@ -1,13 +1,12 @@
 
-    var iSpeed = 20; // time delay of print out
-    var iIndex = 0; // start printing array at this posision
-    var iArrLength = aText[0].length; // the length of the text array
-    var iScrollAt = 20; // start scrolling up at this many lines
-    var iTextPos = 0; // initialise text position
-    var sContents = ''; // initialise contents variable
-    var iRow; // initialise current row
-    var iD = "typedtext";
-    var iLineDelay = 20;
+var iSpeed = 20; // time delay of print out
+var iIndex = 0; // start printing array at this posision
+var iArrLength = aText[0].length; // the length of the text array
+var iScrollAt = 20; // start scrolling up at this many lines
+var iTextPos = 0; // initialise text position
+var sContents = ''; // initialise contents variable
+var iRow; // initialise current row
+var iLineDelay = 20;
 
 function typewriter()
 {
@@ -15,11 +14,15 @@ function typewriter()
     sContents =  ' ';
     iRow = Math.max(0, iIndex-iScrollAt);
     var destination = document.getElementById(iD);
+    if (fullID != '') {
+        var fulltextDestination = document.getElementById(fullID);
+        fulltextDestination.innerHTML = sContents + aText + endText;
+    }
     
     while ( iRow < iIndex ) {
         sContents += aText[iRow++] + '<br />';
     }
-    destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + '<span class="cursor">&block;</span>';
+    destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + endText;
 
     if ( iTextPos++ == iArrLength ) {
         iTextPos = 0;
