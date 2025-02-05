ed011ee75fe52b728bbcad7794364c194aa58929
waverider <unstinkable919@gmail.com>
2025-02-05 11:38:12 -0500

    fix

--- public/js/home.js
+++ public/js/home.js
@@ -1,3 +1,3 @@
 var aText = new Array(
-    "FIRST Tech Challenge team based in Charlotte, North Carolina; under the umbrella of Charlotte STEAMworks, a nonprofit organization which provides services that allow us to manage our own team's finances."
-);
+    "FIRST Tech Challenge team based in Charlotte, North Carolina; under the umbrella of Charlotte STEAMworks"
+)
